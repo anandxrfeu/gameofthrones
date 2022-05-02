ec8445cba35a80a7d68e79c061602e59436a7676
Anand.Tamilchelvan <anandxrfeu@gmail.com>
2022-05-02 20:26:55 -0300

    refactor code

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,115 @@
-//Get a reference to the canvas and output
+//Get a reference dom elements
 var canvas = document.querySelector("#canvas");
-var output = document.querySelector("#output");
+var gameMessageText = document.querySelector("#gameMessage");
+var playBtn = document.getElementById("play")
+var bgAudioBtn = document.getElementById("bgAudioBtn")
+var bgAudio = document.getElementById("bgAudio")
+var minute = document.getElementById('min')
+var second = document.getElementById('sec')
 
-//Add a keyboard listener
-window.addEventListener("keydown", keydownHandler, false);
+playBtn.addEventListener('click',playGame)
+bgAudioBtn.addEventListener('click',toggleMusic)
+
+function toggleMusic(){
+  if(bgAudioBtn.innerHTML === 'Music OFF'){
+    bgAudioBtn.innerHTML = 'Music ON'
+    bgAudio.play()
+  }else{
+    bgAudioBtn.innerHTML = 'Music OFF'
+    bgAudio.pause()
+  }
+}
+
+var seconds = 70
+
+function playGame(){
+
+    if(playBtn.innerHTML === 'replay'){
+      location.reload()
+    }else{
+      //Logic to disable play button
+      playBtn.classList.toggle('play')
+      playBtn.classList.toggle('replay')
+      playBtn.removeEventListener('click',playGame)
+
+      intervalId = setInterval(()=>{
+        seconds--
+        displayTimer()
+        //Add a keyboard listener
+        window.addEventListener("keydown", keydownHandler);
+        // Find start position of Arya and the white walker
+        locatePlayerAndMonster()
+
+      if(seconds === 0){
+        endGame()
+        clearInterval(intervalId)
+        seconds = 70
+        playBtn.classList.toggle('replay')
+        playBtn.classList.toggle('play')
+        playBtn.addEventListener('click',playGame)
+        }
+      },1000)
+    }
+    
+}
+
+function displayTimer(){
+  minute.innerHTML = Math.floor(seconds / 60)
+  second.innerHTML = seconds % 60
+}
+
+
+// class instantiations
+var trail = new Sprite('trail')
+var arya = new Player('arya','/images/arya.png')
+var whiteWalker = new Monster('whiteWalker','/images/white-walker.png')
+var baratheon = new House ('baratheon', '/images/baratheon.png')
+var lannister = new House('lannister','/images/lannister.png')
+var arryn = new House('arryn','/images/arryn.png')
+var martell = new House('martell','/images/martell.png')
+var targaryen = new House('targaryen','/images/targaryen.png')
+var greyjoy = new House('greyjoy','/images/greyjoy.svg')
+var tully = new House('tully','/images/tully.png')
+var tyrell = new House('tyrell','/images/tyrell.png')
+var bolton = new House('bolton','/images/Background.svg')
+var stark = new House('stark','/images/stark.png')
 
 
 /**
- * The game map
- * This is a 2 dimentional array used to create the canvas area of the game
- * This map holds stationary characters of the game, such as the westeros houses
+ * The game board
+ * This is a 2 dimentional array used to create the board area of the game
+ * This board holds stationary characters of the game, such as the westeros houses
  */
 
-var map =
+var board =
 [
-  [0, 2, 0, 0, 0, 2, 0, 3],
-  [0, 0, 0, 1, 0, 0, 0, 0],
-  [0, 1, 0, 0, 0, 0, 1, 0],
-  [0, 0, 0, 0, 2, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 1],
-  [2, 0, 1, 0, 0, 2, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 1, 0, 0]
+  [trail, lannister, trail, trail, trail, targaryen, trail, stark],
+  [trail, trail, trail, arryn, trail, trail, trail, trail],
+  [trail, baratheon, trail, trail, trail, trail, tully, trail],
+  [trail, trail, trail, trail, tyrell, trail, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, greyjoy],
+  [trail, trail, martell, trail, trail, lannister, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, trail],
+  [trail, trail, trail, trail, trail, bolton, trail, trail]
 ];
 
 /*
- * The game objects map
- * This is also a 2D array that overlaps the game map
- * This map holds the moving characters if the game, sich as Arya and the White Walker
+ * The game objects board
+ * This is also a 2D array that overlaps the game board
+ * This board holds the moving characters if the game, sich as Arya and the White Walker
  */
 var gameObjects =
 [
-  [5, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [4, 0, 0, 0, 0, 0, 0, 0]
+  [whiteWalker, trail, trail, trail, trail, trail, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, trail],
+  [trail, trail, trail, trail, trail, trail, trail, trail],
+  [arya, trail, trail, trail, trail, trail, trail, trail]
 ];
 
-//Map code
-var TRAIL = 0;
-var BOOST = 1;
-var OBSTACLE = 2;
-var HOME = 3;
-var PLAYER = 4;
-var MONSTER = 5;
 
 //The size of each cell
 var SIZE = 96;
@@ -55,17 +117,10 @@ var SIZE = 96;
 var SPACE = 0;
 
 //The number of rows and columns
-var ROWS = map.length;
-var COLUMNS = map[0].length;
+var ROWS = board.length;
+var COLUMNS = board[0].length;
 
-//Variables to track the start positions of the Player and the Monster
-var playerRow;
-var playerColumn;
-var monsterRow;
-var monsterColumn;
 
-// Find start position of Arya and the white walker
-locatePlayerAndMonster()
 
 //Arrow key codes
 var UP = 38;
@@ -88,13 +143,14 @@ render();
 function locatePlayerAndMonster(){
   for(var row = 0; row < ROWS; row++) {	
     for(var column = 0; column < COLUMNS; column++) {
-      if(gameObjects[row][column] === PLAYER){ 
-        playerRow = row;
-        playerColumn = column;
+      let spriteObject = gameObjects[row][column]
+      if(spriteObject.getName() === 'arya'){ 
+        arya.playerRow = row;
+        arya.playerColumn = column;
       }
-      if(gameObjects[row][column] === MONSTER){ 
-        monsterRow = row;
-        monsterColumn = column;
+      if(spriteObject.getName() === 'whiteWalker'){ 
+        whiteWalker.monsterRow = row;
+        whiteWalker.monsterColumn = column;
       }
     }
   }
@@ -114,70 +170,80 @@ function keydownHandler(event){
        //And reduce playerRow by 1
        //And set current location of player with new row and column value
 
-	    if(playerRow > 0){
-	      gameObjects[playerRow][playerColumn] = 0;
-	      playerRow--;
-	      gameObjects[playerRow][playerColumn] = PLAYER;
+	    if(arya.playerRow > 0){
+	      gameObjects[arya.playerRow][arya.playerColumn] = new Sprite('trail');
+	      arya.playerRow--;
+	      gameObjects[arya.playerRow][arya.playerColumn] = arya;
 	    }
 	    break;
 	  
 	  case DOWN:
-      if(playerRow < ROWS - 1){
-	      gameObjects[playerRow][playerColumn] = 0;
-	      playerRow++;
-	      gameObjects[playerRow][playerColumn] = PLAYER;
+      if(arya.playerRow < ROWS - 1){
+	      gameObjects[arya.playerRow][arya.playerColumn] = new Sprite('trail');
+	      arya.playerRow++;
+	      gameObjects[arya.playerRow][arya.playerColumn] = arya;
 	    }
 	    break;
 	    
 	  case LEFT:
-	    if(playerColumn > 0){
-	      gameObjects[playerRow][playerColumn] = 0;
-	      playerColumn--;
-	      gameObjects[playerRow][playerColumn] = PLAYER;
+	    if(arya.playerColumn > 0){
+	      gameObjects[arya.playerRow][arya.playerColumn] = new Sprite('trail');
+	      arya.playerColumn--;
+	      gameObjects[arya.playerRow][arya.playerColumn] = arya;
 	    }
 	    break;  
 	    
 	  case RIGHT:
-	    if(playerColumn < COLUMNS - 1){
-	      gameObjects[playerRow][playerColumn] = 0;
-	      playerColumn++;
-	      gameObjects[playerRow][playerColumn] = PLAYER;
+	    if(arya.playerColumn < COLUMNS - 1){
+	      gameObjects[arya.playerRow][arya.playerColumn] = new Sprite('trail');
+	      arya.playerColumn++;
+	      gameObjects[arya.playerRow][arya.playerColumn] = arya;
 	    }
 	    break; 
   }
   
   //find out what kind of cell the PLAYER is on
-  switch(map[playerRow][playerColumn]){
-    case TRAIL:
-      gameMessage = "Arya walks through the kingdoms."
+  let visitingHouse = board[arya.playerRow][arya.playerColumn]
+  switch(visitingHouse.getName()){
+    
+    case 'trail':
+      gameMessage = "Arya wanders through Westeroes."
       break;
     
-    case OBSTACLE:
-      fight();
+    case 'lannister':
+    case 'targaryen':
+    case 'tyrell':
+    case 'bolton':
+    case 'greyjoy':
+      arya.fight(visitingHouse);
       break; 
     
-    case BOOST:
-      trade();
+    case 'baratheon':
+    case 'tully':
+    case 'martell':
+    case 'arryn':
+      arya.trade(visitingHouse);
       break; 
       
-    case HOME:
+    case 'stark':
       endGame();
       break;      
   }
   
   //Move the monster
-  moveMonster();
+  whiteWalker.move()
+  //moveMonster();
   
   //Find out if Arya and WhiteWalker are on the same cell in the gameObjects array
-  if(gameObjects[playerRow][playerColumn] === MONSTER){
+  if(gameObjects[arya.playerRow][arya.playerColumn].getName() === 'whiteWalker'){
     endGame();
   }
   
   //Subtract some food each turn
-  food--;
+  arya.eat()
   
   //Find out if the PLAYER has run out of food or gold
-  if(food <= 0 || gold <= 0)
+  if(!arya.hasFood() || !arya.hasGold())
   {
     endGame();
   }
@@ -186,194 +252,52 @@ function keydownHandler(event){
   render();
 }
 
-/**
- * Function moves the White Walker in a random manner to a nearby cell
- */
-function moveMonster(){
-  //The 4 possible directions that the monster can move
-  var UP = 1;
-  var DOWN = 2;
-  var LEFT = 3;
-  var RIGHT = 4;
-  
-  // An array to save valid directions where the white walker can travel
-  var validDirections = [];
-  
-  // The direction where the white walker will travel. This will be chocen randomly
-  var direction = undefined;
-  
-  if(monsterRow > 0){
-    var thingAbove = map[monsterRow - 1][monsterColumn];
-    if(thingAbove === TRAIL){
-	    validDirections.push(UP);
-	  }
-  }
-  if(monsterRow < ROWS - 1){ 
-    var thingBelow = map[monsterRow + 1][monsterColumn];
-    if(thingBelow === TRAIL){
-	    validDirections.push(DOWN);
-	  }
-  }
-  if(monsterColumn > 0){
-    var thingToTheLeft = map[monsterRow][monsterColumn - 1];
-    if(thingToTheLeft === TRAIL){
-	    validDirections.push(LEFT);
-	  }
-  } 
-  if(monsterColumn < COLUMNS - 1){
-    var thingToTheRight = map[monsterRow][monsterColumn + 1];
-    if(thingToTheRight === TRAIL){
-	    validDirections.push(RIGHT);
-	  }
-  } 
-
-  // Randomly choose a direction for the whiteWalker to travel
-  if(validDirections.length !== 0){
-    var randomNumber = Math.floor(Math.random() * validDirections.length);
-    direction = validDirections[randomNumber];
-  }
-  
-  //Move the WhiteWalker in the chosen direction
-  switch(direction){
-    case UP:
-      gameObjects[monsterRow][monsterColumn] = 0;
-		  monsterRow--; 
-		  gameObjects[monsterRow][monsterColumn] = MONSTER;
-		  break;
-	  
-	  case DOWN:
-	    gameObjects[monsterRow][monsterColumn] = 0;
-		  monsterRow++;
-		  gameObjects[monsterRow][monsterColumn] = MONSTER;
-	    break;
-	  
-	  case LEFT:
-	    gameObjects[monsterRow][monsterColumn] = 0;
-		  monsterColumn--;
-		  gameObjects[monsterRow][monsterColumn] = MONSTER;
-	    break;
-	 
-	 case RIGHT:
-	    gameObjects[monsterRow][monsterColumn] = 0;
-		  monsterColumn++;
-		  gameObjects[monsterRow][monsterColumn] = MONSTER;
-  }
-}
-
-
-/**
- * When Arya enters territory of a house that owes alligience to 
- * House Stark, she trades her gold coins for food and ale
- * Arya also gains experience trading
- */
-function trade(){
-  // Calculate how much food the house has
-  var BOOSTsFood = experience + gold;
-  // Calculate the cost of food
-  var cost = Math.ceil(Math.random() * BOOSTsFood);
-  
-  //Let Arya buy food if she has enough gold
-  if(gold > cost){
-    food += BOOSTsFood;
-    gold -= cost;
-    experience += 2;
-    
-    gameMessage 
-      = "Arya bought " + BOOSTsFood + " pieces of bread"
-      + " for " + cost + " gold coins."
-  }else{
-    // Arya does not have enough gold to buy food
-     experience += 1;
-     gameMessage = "Arya doesn't have enough gold to buy food."
-  }
-}
-
-/**
- * When Arya enteres a territory of an enemy house, she has to fight
- * If Arya wins, she gets half of the enemies gold
- * If Arya looses, she looses all or part of her gold
- * In either case, Arya gains experience
- */
-function fight(){
-  
-  //Arya's strength
-  var PLAYERStrength = Math.ceil((food + gold) / 2);
-  
-  //House`s strength is a random number between 1 and the Arya's strength
-  var OBSTACLEStrength = Math.ceil(Math.random() * PLAYERStrength * 2);
-  
-  // Opponent House Wins
-  if(OBSTACLEStrength > PLAYERStrength){
-    var stolenGold = Math.round(OBSTACLEStrength / 2);
-    gold -= stolenGold;
-
-    //Arya gains experience
-    experience += 1;
-    
-    //Update the game message
-    gameMessage 
-    = "Arya fought and LOST " + stolenGold + " gold pieces."
-
-  }else{
-    // Arya wins
-    var OBSTACLEGold = Math.round(OBSTACLEStrength / 2);
-    gold += OBSTACLEGold;
-    
-    //Arya gains experience  
-    experience += 2;
-    
-    //Update the game message
-    gameMessage 
-      = "Arya fought and WON " + OBSTACLEGold + " gold pieces."
-  } 
-}
-
 function endGame()
 {
-  if(map[playerRow][playerColumn] === HOME)
+  if(board[arya.playerRow][arya.playerColumn].getName() === 'stark')
   {
     //Calculate the score
-    var score = food + gold + experience;
+    var score = arya.food + arya.gold + arya.experience;
     
     //Display the game message
     gameMessage 
-      = "Arya has made it to winterfell ALIVE! " + "<br>Final Score: " + score; 
-  }
-  else if(gameObjects[playerRow][playerColumn] === MONSTER)
-  {
+      = "Arya has made it to winterfell ALIVE!<br>"; 
+  }else if(gameObjects[arya.playerRow][arya.playerColumn].getName()  === 'whiteWalker'){
     gameMessage 
-      = "Arya has been killed by a White Walker!";
-  }
-  else
-  {
+      = "Arya has been killed by a White Walker!<br>";
+  }else{
     //Display the game message
-    if(gold <= 0)
-    {
-      gameMessage = " <br>Arya runs out of gold!"; 
-    }
-    else
-    {
-      gameMessage = " <br>Arya runs out of food!"; 
+    if(arya.gold <= 0){
+      gameMessage = "Arya runs out of gold, and is killed by her squire!"; 
+    }else{
+      gameMessage = "Arya runs out of food!, and dies a painfull death!"; 
     }
     
-    gameMessage 
-      += " , and dies a painfull death!"; 
+//    gameMessage += ", and dies a painfull death!"; 
   }
   
   //Remove the keyboard listener to end the game
   window.removeEventListener("keydown", keydownHandler, false);
+
+  // Need to come back to this
+  clearInterval(intervalId)
+  displayTimer()
+  playBtn.classList.toggle('replay')
+  playBtn.classList.toggle('play')
+  playBtn.innerHTML='replay'
+  playBtn.addEventListener('click',playGame)
 }
 
 function render()
 {
-  //Clear the canvas of img cells from the previous turn
+  //Clear the board of img cells from the previous turn
   if(canvas.hasChildNodes()){
     for(var i = 0; i < ROWS * COLUMNS; i++) {	 
       canvas.removeChild(canvas.firstChild);
     }
   }
   
-  //Render the canvas by looping through the map arrays
+  //Render the board by looping through the board arrays
   for(var row = 0; row < ROWS; row++){	
     for(var column = 0; column < COLUMNS; column++){ 
       //Create a img tag called cell
@@ -385,33 +309,63 @@ function render()
       //Add the img tag to the <div id="canvas"> tag
       canvas.appendChild(cell);
 
-      //Find the correct image for this map cell
-      switch(map[row][column]){
-        case TRAIL:
-        cell.style.backgroundColor = 'white'
-        break;
+      //Find the correct image for this board cell
+      let spriteObject = board[row][column]
+      switch(spriteObject.getName()){
+        case 'trail':
+          cell.style.backgroundColor = 'white'
+          break;
 
-        case BOOST:
-            cell.src = '/images/boost.png'
+        case 'baratheon':
+          cell.src = baratheon.bannerImage
           break; 
 
-        case OBSTACLE:
-            cell.src = '/images/obstacle.png'
+        case 'lannister':
+          cell.src = lannister.bannerImage
           break; 
 
-        case HOME:
-            cell.src = '/images/home.png'
+        case 'stark':
+          cell.src = stark.bannerImage
           break;   
+
+        case 'arryn':
+          cell.src = arryn.bannerImage
+          break;
+
+        case 'martell':
+          cell.src = martell.bannerImage
+          break;
+
+        case 'targaryen':
+          cell.src = targaryen.bannerImage
+          break;
+
+        case 'tully':
+          cell.src = tully.bannerImage
+          break;
+
+        case 'tyrell':
+          cell.src = tyrell.bannerImage
+          break;         
+        
+        case 'bolton':
+          cell.src = bolton.bannerImage
+          break;  
+          
+        case 'greyjoy':
+          cell.src = greyjoy.bannerImage
+          break;
       }  
       
       //Add the PLAYER and monster from the gameObjects array
-	    switch(gameObjects[row][column]){
-	      case PLAYER:
-	        cell.src = '/images/arya.png'
+      spriteObject = gameObjects[row][column]
+	    switch(spriteObject.getName()){
+	      case 'arya':
+	        cell.src = arya.playerImage
 	        break;   
 	        
-	      case MONSTER:
-	        cell.src = '/images/ww.png'
+	      case 'whiteWalker':
+	        cell.src = whiteWalker.monsterImage
 	        break;  
 	    } 
   
@@ -422,10 +376,11 @@ function render()
   }
   
   //Display the game message
-	output.innerHTML = gameMessage;
+	gameMessageText.innerHTML = gameMessage;
 	
 	//Display the player's food, gold, and experience
-	output.innerHTML 
-	  += "<br>Gold: " + gold + ", Food: " 
-	  + food + ", Experience: " + experience;
+	gameMessageText.innerHTML 
+	  += "<br>Gold: " + arya.gold + ", Food: " 
+	  + arya.food + ", Experience: " + arya.experience;
 }
+
